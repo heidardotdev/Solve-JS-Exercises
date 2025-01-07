27b9b77484f6f0659da28ab7680e47b5554da9a1
Heidar.Dev <165137772+heidardotdev@users.noreply.github.com>
2025-01-07 22:21:57 +0330

    🔰feat: solve CaptchaCode exercise

---
+++ exersice - captchaCode/scripts/script.js
@@ -0,0 +1,6 @@
+// پروژه ای بنویسین که کد کپچای 5 رقمی به صورت تصادفی تولید کند
+
+var randomNumber = Math.random() * 100000
+var captchaNumber = Math.floor(randomNumber)
+alert ("⚡Your Captcha is:   " + captchaNumber)
+
