7c24ad8572f26cf45412d5ef59b51f69b53f83d0
Heidar.Dev <165137772+heidardotdev@users.noreply.github.com>
2025-01-07 16:01:48 +0330

    🔰feat: solve averageCalculato function exercise

---
+++ exersice - function averageCalculator/scripts/script.js
@@ -0,0 +1,9 @@
+// تابعی بنویسید که سه عدد رو بگیره و میانگین اونا رو محاسبه کرده و نشون بده
+
+function averageCalculator (firstNumber, secondNumber, thirdNumber){
+    var avrage = (firstNumber + secondNumber + thirdNumber) / 3
+    alert(avrage)
+}
+averageCalculator(20, 20, 20)
+averageCalculator(50, 50, 50)
+averageCalculator(19.23, 19.86, 20)
